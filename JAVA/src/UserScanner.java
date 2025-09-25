f71df5e8b3d4effb3112d15baf47e3523dca3efe
이자목 <jmwasd3412@gmail.com>
2025-09-25 14:28:36 +0900

    .

---
+++ JAVA/src/UserScanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class UserScanner {
+    public class UseSanner {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter youer name: ");
+            String name = scanner.nextLine();
+            System.out.println("Hello, " + name + "!");
+            int old = scanner.nextInt();
+            System.out.println("당신의 나이는 : "+old);
+            scanner.close();
+        }
+    }
+
+}
