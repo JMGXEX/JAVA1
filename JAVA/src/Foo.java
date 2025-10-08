2e7ab277cd49c561820b564dac89db6b99516771
이자목 <jmwasd3412@gmail.com>
2025-10-09 00:08:13 +0900

    .

--- JAVA/src/Foo.java
+++ JAVA/src/Foo.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
 public class Foo {
+
     public static void main(String[] args) {
         byte b = 127;
         int i = 100;
 
-        System.out.println(b+i);
-        System.out.println(10/4);
-        System.out.println(10.0/4);
-        System.out.println((char)0x12340061);
-        System.out.println((int)2.9 + (int)1.8);
-        System.out.println((int)(2.9+1.8));
-    }
+//        System.out.println(b+i);
+//        System.out.println(10/4);
+//        System.out.println(10.0/4);
+//        System.out.println((char)0x12340061);
+//        System.out.println((int)2.9 + (int)1.8);
+//        System.out.println((int)(2.9+1.8));
+
+
+       }
 }
